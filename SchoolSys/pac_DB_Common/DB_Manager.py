3580dbd3ca056cfe0330bdf81a8240e53cdf36a4
Rich <liujh88888@sina.cn>
2019-03-15 12:35:08 +0800

    数据库模块开发完成

--- SchoolSys/pac_DB_Common/DB_Manager.py
+++ SchoolSys/pac_DB_Common/DB_Manager.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 from pac_school import School
 from pac_class import Class
+from pac_teacher import Teacher
+from pac_student import Student
+from pac_subject import Subject
 
 
 class DB_Manager:
@@ -142,4 +145,190 @@ class Class_DB(DB_Manager):
         }
         self.get_session().query(self.classes).filter(
             self.classes.class_code == obj.code).update(attrs)
+        self.get_session().commit()
+
+
+class Teacher_DB(DB_Manager):
+
+    def __init__(self):
+        super().__init__()
+
+    '向数据库添加一个教师'
+
+    def add_teacher(self, objTeacher):
+        dbTeacher = self.teacher(teacher_code=objTeacher.code,
+                                 teacher_name=objTeacher.name, school_code=objTeacher.school_code)
+        self.get_session().add(dbTeacher)
+        self.get_session().commit()
+
+    '从数据库获取全部教师信息'
+
+    def get_all_teacher(self):
+        teacher_result = self.get_session().query(self.teacher).all()
+        _dicTeacher = {}
+        for obj in teacher_result:
+            objTeacher = Teacher.Teacher()
+            objTeacher.code = obj.teacher_code
+            objTeacher.name = obj.teacher_name
+            objTeacher.school_code = obj.school_code
+            _dicTeacher.update({obj.teacher_code: objTeacher})
+        return _dicTeacher
+
+    '根据教师编码，从数据库获取指定的教师信息'
+
+    def get_teacher_by_code(self, code):
+        teacher_result = self.get_session().query(
+            self.teacher).filter(self.teacher.teacher_code == code)
+        _dicTeacher = {}
+        for obj in teacher_result:
+            objTeacher = Teacher.Teacher()
+            objTeacher.code = obj.teacher_code
+            objTeacher.name = obj.teacher_name
+            objTeacher.school_code = obj.school_code
+            _dicTeacher.update({obj.teacher_code: objTeacher})
+        return _dicTeacher
+
+    '根据教师编码，从数据库删除指定的教师信息'
+
+    def delete_teacher(self, code):
+        self.get_session().query(self.teacher).filter(
+            self.teacher.teacher_code == code).delete()
+        self.get_session().commit()
+
+    def edit_teacher(self, obj):
+        attrs = {
+            "teacher_name": obj.name
+        }
+        self.get_session().query(self.teacher).filter(
+            self.teacher.teacher_code == obj.code).update(attrs)
+        self.get_session().commit()
+
+
+class Student_DB(DB_Manager):
+
+    def __init__(self):
+        super().__init__()
+
+    '向数据库添加一个学生'
+
+    def add_student(self, objStudent):
+        dbStudent = self.student(
+            student_code=objStudent.code,
+            student_name=objStudent.name,
+            school_code=objStudent.school_code,
+            class_code=objStudent.class_code,
+            student_sex=objStudent.sex,
+            student_age=objStudent.age
+        )
+        self.get_session().add(dbStudent)
+        self.get_session().commit()
+
+    '从数据库获取全部学生信息'
+
+    def get_all_student(self):
+        student_result = self.get_session().query(self.student).all()
+        _dicStudent = {}
+        for obj in student_result:
+            objStudent = Student.Student()
+            objStudent.code = obj.student_code
+            objStudent.name = obj.student_name
+            objStudent.school_code = obj.school_code
+            objStudent.class_code = obj.class_code
+            objStudent.age = obj.student_age
+            objStudent.sex = obj.student_sex
+            _dicStudent.update({obj.student_code: objStudent})
+        return _dicStudent
+
+    '根据学生编码，从数据库获取指定的学生信息'
+
+    def get_student_by_code(self, code):
+        student_result = self.get_session().query(
+            self.student).filter(self.student.student_code == code)
+        _dicStudent = {}
+        for obj in student_result:
+            objStudent = Student.Student()
+            objStudent.code = obj.student_code
+            objStudent.name = obj.student_name
+            objStudent.school_code = obj.school_code
+            objStudent.class_code = obj.class_code
+            objStudent.age = obj.student_age
+            objStudent.sex = obj.student_sex
+            _dicStudent.update({obj.student_code: objStudent})
+        return _dicStudent
+
+    '根据学生编码，从数据库删除指定的学生信息'
+
+    def delete_student(self, code):
+        self.get_session().query(self.student).filter(
+            self.student.student_code == code).delete()
+        self.get_session().commit()
+
+    def edit_student(self, obj):
+        attrs = {
+            "student_name": obj.name,
+            "student_age": obj.age,
+            "student_sex": obj.sex,
+            "class_code": obj.class_code,
+            "school_code": obj.school_code
+        }
+        self.get_session().query(self.student).filter(
+            self.student.student_code == obj.code).update(attrs)
+        self.get_session().commit()
+
+
+
+
+class Subject_DB(DB_Manager):
+
+    def __init__(self):
+        super().__init__()
+
+    '向数据库添加一个课程'
+
+    def add_subject(self, objSubject):
+        dbSubject = self.subject(
+            subject_code=objSubject.code,
+            subject_name=objSubject.name,
+        )
+        self.get_session().add(dbSubject)
+        self.get_session().commit()
+
+    '从数据库获取全部课程信息'
+
+    def get_all_subject(self):
+        subject_result = self.get_session().query(self.subject).all()
+        _dicSubject = {}
+        for obj in subject_result:
+            objSubject = Subject.Subject()
+            objSubject.code = obj.subject_code
+            objSubject.name = obj.subject_name
+            _dicSubject.update({obj.subject_code: objSubject})
+        return _dicSubject
+
+    '根据课程编码，从数据库获取指定的课程信息'
+
+    def get_subject_by_code(self, code):
+        subject_result = self.get_session().query(
+            self.subject).filter(self.subject.subject_code == code)
+        _dicSubject = {}
+        for obj in subject_result:
+            objSubject = Subject.Subject()
+            objSubject.code = obj.subject_code
+            objSubject.name = obj.subject_name
+            _dicSubject.update({obj.subject_code: objSubject})
+        return _dicSubject
+
+    '根据课程编码，从数据库删除指定的课程信息'
+
+    def delete_subject(self, code):
+        self.get_session().query(self.subject).filter(
+            self.subject.subject_code == code).delete()
+        self.get_session().commit()
+
+    def edit_subject(self, obj):
+        attrs = {
+            "subject_name": obj.name
+        }
+        self.get_session().query(self.subject).filter(
+            self.subject.subject_code == obj.code).update(attrs)
         self.get_session().commit()
