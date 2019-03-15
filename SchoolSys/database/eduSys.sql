796a0888a009658f0ca4e4fd8b5e7b6fb85cd834
Rich <liujh88888@sina.cn>
2019-03-15 12:45:50 +0800

    更新数据库

--- SchoolSys/database/eduSys.sql
+++ SchoolSys/database/eduSys.sql
@@ -1,39 +1,54 @@
-/*
- Navicat MySQL Data Transfer
+-- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
+--
+-- Host: localhost    Database: edusys
+-- ------------------------------------------------------
+-- Server version	8.0.13
 
- Source Server         : python_db
- Source Server Type    : MySQL
- Source Server Version : 50641
- Source Host           : 101.132.109.255:3306
- Source Schema         : eduSys
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 50641
- File Encoding         : 65001
+--
+-- Table structure for table `classes`
+--
 
- Date: 11/03/2019 17:58:03
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for classes
--- ----------------------------
 DROP TABLE IF EXISTS `classes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `classes` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `class_name` varchar(255) NOT NULL,
   `class_code` varchar(255) NOT NULL,
   `school_code` varchar(255) NOT NULL,
-  `addr` varchar(255) NOT NULL,
+  `class_addr` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for lesson
--- ----------------------------
+--
+-- Dumping data for table `classes`
+--
+
+LOCK TABLES `classes` WRITE;
+/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
+INSERT INTO `classes` VALUES (1,'go fuck you self','133','010','fuck you');
+/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lesson`
+--
+
 DROP TABLE IF EXISTS `lesson`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `lesson` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `teacher_code` varchar(255) NOT NULL,
@@ -44,67 +59,127 @@ CREATE TABLE `lesson` (
   `score` int(8) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for school
--- ----------------------------
+--
+-- Dumping data for table `lesson`
+--
+
+LOCK TABLES `lesson` WRITE;
+/*!40000 ALTER TABLE `lesson` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lesson` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `school`
+--
+
 DROP TABLE IF EXISTS `school`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `school` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `school_name` varchar(255) NOT NULL,
   `school_code` varchar(255) NOT NULL,
   `school_addr` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of school
--- ----------------------------
-BEGIN;
-INSERT INTO `school` VALUES (1, '清华大学', '003', '北京');
-INSERT INTO `school` VALUES (2, '浙江大学', '004', '杭州');
-INSERT INTO `school` VALUES (3, '大红鹰学院', '009', '宁波');
-INSERT INTO `school` VALUES (4, '测试大学', '009', '测试地址');
-INSERT INTO `school` VALUES (5, '测试大学', '009', '测试地址');
-INSERT INTO `school` VALUES (6, '我的大学', '110', '我的大学地址');
-COMMIT;
+--
+-- Dumping data for table `school`
+--
 
--- ----------------------------
--- Table structure for student
--- ----------------------------
+LOCK TABLES `school` WRITE;
+/*!40000 ALTER TABLE `school` DISABLE KEYS */;
+INSERT INTO `school` VALUES (1,'清华大学','003','北京'),(2,'浙江大学','004','杭州'),(3,'大红鹰学院','009','宁波'),(4,'测试大学','009','测试地址'),(5,'测试大学','009','测试地址'),(6,'我的大学','110','我的大学地址'),(9,'北京大学','010','北京');
+/*!40000 ALTER TABLE `school` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
 DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `student` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `student_code` varchar(255) NOT NULL,
   `class_code` varchar(255) NOT NULL,
   `school_code` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `sex` varchar(8) NOT NULL,
-  `age` int(4) DEFAULT NULL,
+  `student_name` varchar(255) NOT NULL,
+  `student_sex` varchar(8) NOT NULL,
+  `student_age` int(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for subject
--- ----------------------------
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subject`
+--
+
 DROP TABLE IF EXISTS `subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `subject` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `subject_code` varchar(255) NOT NULL,
   `subject_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for teacher
--- ----------------------------
+--
+-- Dumping data for table `subject`
+--
+
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teacher`
+--
+
 DROP TABLE IF EXISTS `teacher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `teacher` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `teacher_code` varchar(255) NOT NULL,
   `school_code` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `teacher_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Dumping data for table `teacher`
+--
+
+LOCK TABLES `teacher` WRITE;
+/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-15 12:43:06
